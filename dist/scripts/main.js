4fef915a86043ceff14a7249d959aa4f6a662b6d
Zach Chaney <zachary.chaney@gmail.com>
2015-06-01 22:14:41 -0500

    doing the bigtime adding that gametime

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -8,37 +8,115 @@ $(document).ready(function () {
 	        $('nav').delay(4500).fadeIn('fast');
 	    });
 
+	$('#playBtn').click(function () {
+				window.location.href = "#play";
+		});
 
+	$('#leaderBtn').click(function () {
+				window.location.href = "#leader";
+		});
+
+	$('#settingsBtn').click(function () {
+				window.location.href = "#settings";
+		});
 
 	var routerConfig = {
 		routes: {
-			'': 'test',
-			'home': 'test',
-			'about': 'test2'
+			'': 'menu',
+			'menu': 'menu',
+			'play': 'play',
+			'leader': 'leaderBoard',
+			'settings': 'settings',
 		},
 
-		test: function() {
-			console.log('test');
+		menu: function() {
+			console.log('menu test');
 			$('.page').hide();
-			$('#a').show();
-
+			$('#menu').show();
 		},
 
-		test2: function() {
+		play: function() {
 			console.log('test 2');
 			$('.page').hide();
-			$('#b').show();
+			$('#menu').show();
+			$('#play').show();
+		},
 
-		}
+		leaderBoard: function() {
+			console.log('test 3');
+			$('.page').hide();
+			$('#menu').show();
+			$('#leader').show();
+		},
+
+		settings: function() {
+			console.log('test 4');
+			$('.page').hide();
+			$('#menu').show();
+			$('#settings').show();
+		},
 	}
-var app = Backbone.Router.extend(routerConfig);
+	
+	var app = Backbone.Router.extend(routerConfig);
 
-var myRouter = new app();
-Backbone.history.start();
+	var myRouter = new app();
+	Backbone.history.start();
+
+//below is my stolen video game code. because world wide webz.
+
+var counter = 0;
+var seconds = 0;
+$('#counter span').html(counter);
+$('#timer span').html(seconds);
+
+$('#start').hide();
+$('#win').hide();
+$('#endScreen').hide();
+
+$('#startGame').on( "mouseover", function() {
+  $(this).hide();
+  
+  var randWidth = random = Math.ceil(Math.random() * 350);
+  var randHeight = random = Math.ceil(Math.random() * 350);
+  
+  $('#win').show('fast');
+  $('#win').css('top', randHeight);
+  $('#win').css('left', randWidth);
+  
+   window.setInterval(function(){
+      seconds = seconds + 1;
+      $('#timer span').html(seconds);
+    }, 1000); 
+});
+
+$('#start').on( "mouseover", function() {
+  
+  var randWidth = random = Math.ceil(Math.random() * 350);
+  var randHeight = random = Math.ceil(Math.random() * 350);
+  
+  $('#win').show('fast');
+  $('#win').css('top', randHeight);
+  $('#win').css('left', randWidth);
+  $(this).hide('fast');
+});
+
+$('#win').on( "mouseover", function() {
+  counter = counter + 1;
+  $('#counter span').html(counter);
+  
+  if(counter == 10){
+  $('#endScreen').show('medium');
+    $('#endCount').html(counter);
+    $('#endSeconds').html(seconds);
+  }
+  
+  $(this).hide('fast');
+  $('#start').show('fast');
+  
+});
 
 
-$('profile-button').click(function(e) {
-	myRouter.navigate('console log profile button');
-})
+
+
 
 });
