fbfabc83005b19eea9b9bfb85a39001441380dd3
Zach Chaney <zachary.chaney@gmail.com>
2015-06-02 00:48:26 -0500

    final commit

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -66,58 +66,58 @@ $(document).ready(function () {
 	var myRouter = new app();
 	Backbone.history.start();
 
-//below is my stolen video game code. because world wide webz.
+//below is stolen video game code because world wide webz.
 
-var counter = 0;
-var seconds = 0;
-$('#counter span').html(counter);
-$('#timer span').html(seconds);
+// var counter = 0;
+// var seconds = 0;
+// $('#counter span').html(counter);
+// $('#timer span').html(seconds);
 
-$('#start').hide();
-$('#win').hide();
-$('#endScreen').hide();
+// $('#start').hide();
+// $('#win').hide();
+// $('#endScreen').hide();
 
-$('#startGame').on( "mouseover", function() {
-  $(this).hide();
+// $('#startGame').on( "mouseover", function() {
+//   $(this).hide();
   
-  var randWidth = random = Math.ceil(Math.random() * 350);
-  var randHeight = random = Math.ceil(Math.random() * 350);
+//   var randWidth = random = Math.ceil(Math.random() * 350);
+//   var randHeight = random = Math.ceil(Math.random() * 350);
   
-  $('#win').show('fast');
-  $('#win').css('top', randHeight);
-  $('#win').css('left', randWidth);
+//   $('#win').show('fast');
+//   $('#win').css('top', randHeight);
+//   $('#win').css('left', randWidth);
   
-   window.setInterval(function(){
-      seconds = seconds + 1;
-      $('#timer span').html(seconds);
-    }, 1000); 
-});
+//    window.setInterval(function(){
+//       seconds = seconds + 1;
+//       $('#timer span').html(seconds);
+//     }, 1000); 
+// });
 
-$('#start').on( "mouseover", function() {
+// $('#start').on( "mouseover", function() {
   
-  var randWidth = random = Math.ceil(Math.random() * 350);
-  var randHeight = random = Math.ceil(Math.random() * 350);
+//   var randWidth = random = Math.ceil(Math.random() * 350);
+//   var randHeight = random = Math.ceil(Math.random() * 350);
   
-  $('#win').show('fast');
-  $('#win').css('top', randHeight);
-  $('#win').css('left', randWidth);
-  $(this).hide('fast');
-});
+//   $('#win').show('fast');
+//   $('#win').css('top', randHeight);
+//   $('#win').css('left', randWidth);
+//   $(this).hide('fast');
+// });
 
-$('#win').on( "mouseover", function() {
-  counter = counter + 1;
-  $('#counter span').html(counter);
+// $('#win').on( "mouseover", function() {
+//   counter = counter + 1;
+//   $('#counter span').html(counter);
   
-  if(counter == 10){
-  $('#endScreen').show('medium');
-    $('#endCount').html(counter);
-    $('#endSeconds').html(seconds);
-  }
+//   if(counter == 10){
+//   $('#endScreen').show('medium');
+//     $('#endCount').html(counter);
+//     $('#endSeconds').html(seconds);
+//   }
   
-  $(this).hide('fast');
-  $('#start').show('fast');
+//   $(this).hide('fast');
+//   $('#start').show('fast');
   
-});
+// });
 
 
 
